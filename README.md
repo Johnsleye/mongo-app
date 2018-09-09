3ecf3dde755aed95ea959f12e73c650e5a7d88c1
Johnsleye <johnsleye072@gmail.com>
2018-09-08 22:30:19 -0400

    mongo app

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mongo-app
+NEWS SCRAPER APP!!!
+
+This App scrap world news from huffpost world news. It renders the headlines with the pictures and a space for the reader to comment. Reader can also delete comments.
+
+This App was made using the following packages:
+Express
+Mongoose
+Anxios
+Cheerio
+Nodejs
+JavaScript
+jQuery
+HTML
+CSS
+Bootstrap.
+
+HAPPY READING!!!
+
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const request = require("request");
+require('dotenv').config()
+
+const db = require("./models");
+
+var PORT = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+axios.get("https://www.huffingtonpost.com/section/world-news").then(function (response) {
+    const $ = cheerio.load(response.data);
+    $('div.card--original-center').each(function (i, element) {
+        let result = {};
+
+        result.title = $(this).children('div.card__content').children('div.card__details').children('.card__headlines').children('.card__headline').text().trim();
+        result.link = 'https://www.huffingtonpost.com' + $(this).children('div.card__content').children('a.card__image__wrapper').attr('href').trim();
+
+        let image = $(this).children('div.card__content').children('a.card__image__wrapper').children('.card__image')
+            .children('img').attr('src').trim().split("?cache")[0];
+
+        image.includes("?ops=") ? result.image = image.split("?ops=")[0] : result.image = image;
+
+        db.Article.create(result)
+            .then(function (dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function (err) {
+                return err;
+            })
+    });
+})
+app.get("/", (req, res) =>
+    db.Article.find({}).sort({
+        _id: -1
+    })
+    .then((dbArticle) => res.json(dbArticle))
+    .catch((err) => res.json(err)));
+
+app.get("/articles", (req, res) =>
+    db.Article.find({}).sort({
+        _id: -1
+    })
+    .then((dbArticle) => res.json(dbArticle))
+    .catch((err) => res.json(err)));
+
+app.get("/articles/:id", (req, res) =>
+    db.Article.findOne({
+        _id: req.params.id
+    })
+    .populate("comments")
+    .then((dbArticle) => res.json(dbArticle))
+    .catch((err) => res.json(err)));
+
+app.get("/comments", (req, res) =>
+    db.Comment.find({})
+    .then((dbNote) => res.json(dbNote))
+    .catch((err) => res.json(err)));
+
+app.get("/articlespopulated", (req, res) =>
+    db.Article.find({}).sort({
+        _id: -1
+    })
+    .populate(["comments"])
+    .then((dbArticle) => res.json(dbArticle))
+    .catch((err) => res.json(err)));
+
+app.post("/articles/:id", function (req, res) {
+    db.Comment.create(req.body)
+        .then(function (dbComment) {
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            },
+            { $push: {comments: dbComment._id}} 
+        )
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.delete("/comments/:id", function(req, res) {
+    db.Comment.deleteOne({_id: req.body.id}, function(err,res){
+        if (err) return err
+    })
+})
+
+app.listen(PORT, () => console.log(`app running on localhost:${PORT}`))
